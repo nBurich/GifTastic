1b08f3966f57cfe42c7060d1765e687160412aa5
nBurich <n.burich@hotmail.com>
2019-04-26 23:18:42 -0400

    updated

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -2,8 +2,7 @@ var topics = [];
 
 $("button").on("click", function () {
 
-    var x = $(this).data("searchInput");
-    console.log(x);
+    var x = $(this).data("data-search");
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=kT8jiIRQ7hEFLmDDvL4QK8Of3q6Sffvm&limit=10";
 
@@ -14,45 +13,47 @@ $("button").on("click", function () {
         var results = response.data;
 
         for (var i = 0; i < results.length; i++) {
+            var showDiv = $("<div class='col-md-4'>");
 
-            var gifDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[i].rating);
-            var gifImage = $("<img>");
-
-            gifImage.attr("src", results[i].images.fixed_height.url);
-            gifDiv.append(p);
-            gifDiv.append(gifImage);
-
-            $("#gifArea").prepend(gifDiv);
+            var rating = results[i].rating;
+            var defaultAnimatedSrc = results[i].images.fixed_height.url;
+            var staticSrc = results[i].images.fixed_height_still.url;
+            var showImage = $("<img>");
+            var p = $("<p>").text("Rating: " + rating);
+    
+            showImage.attr("src", staticSrc);
+            showImage.addClass("displayedImg");
+            showImage.attr("data-state", "still");
+            showImage.attr("data-still", staticSrc);
+            showImage.attr("data-animate", defaultAnimatedSrc);
+            showDiv.append(p);
+            showDiv.append(showImage);
+            $("#gifArea").prepend(showDiv);
         }
     });
 });
-function button() {
-
-
-    $("#buttons-view").empty();
-
-    // Looping through the array of movies
-    for (var i = 0; i < topics.length; i++) {
-
-
-        var a = $("<button>");
-        a.addClass("movie-btn");
-        a.attr("data-name", topics[i]);
-
-        a.text(topics[i]);
-        $("#buttons-view").append(a);
-    }
-    console.log(topics);
-}
-
-
 $("#addInput").on("click", function (event) {
     event.preventDefault();
-
     var newStuff = $("#searchInput").val().trim();
-    newStuff.push(topics);
+    topics.push(newStuff);
+    console.log(topics);
+    $("#searchInput").val('');
+    displayButtons();
+  });
 
-    //makes button from array
-    button();
-});
+  function displayButtons() {
+    $("#myButtons").empty();
+    for (var i = 0; i < topics.length; i++) {
+      var a = $('<button class="btn btn-primary">');
+      a.attr("id", "giff");
+      a.attr("data-search", topics[i]);
+      a.text(topics[i]);
+      $("#myButtons").append(a);
+    }
+  }
+
+
+  displayButtons();
+
+
+
